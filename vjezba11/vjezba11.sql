b8c62ca97f4ca804074cb4730a407e9312ca7e7b
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-05-31 15:38:26 +0200

    rješenja do 4 zadatka

--- vjezba11/vjezba11.sql
+++ vjezba11/vjezba11.sql
@@ -87,6 +87,44 @@ alter table djevojka add foreign key (svekrva) references svekrva(sifra);
 alter table punica add foreign key (djevojka) references djevojka(sifra);
 
 
+insert into punac (sifra,kuna,vesta) values
+(null,15.9,'zelena'),(null,1.9,'crna'),
+(null,25.9,'plava');
+
+insert into mladic(sifra,ogrlica,drugiputa,hlace) values
+(null,3,2022-06-06,'plava'),(null,3,2021-06-06,'zelena'),
+(null,3,2020-06-06,'crna');
+
+insert into punacmladic(sifra,punac,mladic) values
+(null,1,1),(null,2,2),(null,3,3);
+
+insert into svekrva(sifra,narukvica,carape,punac) values
+(null,3,'plava',1),(null,1,'crna',3),(null,18,'zelena',2);
+
+insert into neprijatelj(sifra,narukvica,novcica,bojakose,introvertno) values
+(null,2,15.3,'crna',null),(null,3,6.3,'crvena',null),(null,58,5.3,'plava',null);
+
+insert into muskarac(sifra,narukvica,neprijatelj) values
+(null,3,1),(null,2,31),(null,15,3);
+
+insert into djevojka(sifra,kratkamajica,ekstroventno,majica,introvertno) values
+(null,'plava',null,'plava',null),(null,'crna',null,'plava',null),
+(null,'zelena',null,'crna',null);
+
+insert into punica(sifra,carape,majica,haljina,bojakose,djevojka) values
+(null,'zelena','plava','crna','zuta',1),
+(null,'zelena','plava','crna','zuta',2),
+(null,'zelena','plava','crna','zuta',3);
+
+update muskarac set novcica=15.70;
+
+delete from punica where drugiputa=2019-04-08;
+
+select haljina from svekrva where carape='%a%n%a%';
+
+
+
+
 
 
 
