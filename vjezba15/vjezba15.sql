3dc8224739a0a2fce9834b8407ddc0cd2fe2082f
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-05-31 16:50:00 +0200

    riješeno do 4 zadatka

--- vjezba15/vjezba15.sql
+++ vjezba15/vjezba15.sql
@@ -82,6 +82,48 @@ alter table prijatelj add foreign key (zarucnica) references zarucnica(sifra);
 alter table decko add foreign key (prijatelj) references prijatelj(sifra);
 
 
+insert into brat(sifra,bojakose,vesta) values
+(null,'plava','crna'),(null,'crvena','zuta'),
+(null,'zuta','bijela');
+
+insert into prijateljica(sifra,kratkamajica,asocijalno,treciputa,jmbag,vesta) values
+(null,'zelena',null,2022-06-06,12345678901,'plava'),
+(null,'zuta',null,2021-06-06,12345688901,'zuta'),
+(null,'crna',null,2020-06-06,12345678301,'zelena');
+
+insert into ostavljena(sifra,stilfrizura) values
+(null,'duga'),(null,'kratka'),(null,'celava');
+
+insert into zarucnik(sifra,kratkamajica,jmbag,dukserica,treciputa) values
+(null,'zelena',12345678901,'plava',2022-06-06),
+(null,'crna',12345678301,'bijela',2012-06-06),
+(null,'zuta',13345678901,'crvena',2020-06-06);
+
+insert into zarucnikostavljena(sifra,zarucnik,ostavljena) values
+(null,1,1),(null,3,2),(null,2,3);
+
+insert into zarucnica(sifra,prviputa,bojaociju) values
+(null,2022-06-06,'plava'),(null,2002-06-06,'zuta'),
+(null,2023-06-06,'crna');
+
+insert into prijatelj(sifra,majica,narukvica) values
+(null,'zuta',3),(null,'plava',33),(null,'crna',16);
+
+insert into decko(sifra,vesta,prijatelj) values
+(null,'plava',1),(null,'crna',2),(null,'zuta',3);
+
+update prijateljica set bojakose='Osijek';
+
+delete from decko where bojakose='%a%b%';
+
+select modelnaocala from zarucnica where bojakose='%a%n%a%';
+
+
+
+
+
+
+
 
 
 
